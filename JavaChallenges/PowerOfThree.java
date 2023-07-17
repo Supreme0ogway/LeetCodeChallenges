4487acf5f422a66e184cea672676e40d9353a6c1
William Lattus <wslattus@gmail.com>
2023-07-17 15:37:59 -0700

    pow of 3

---
+++ JavaChallenges/PowerOfThree.java
@@ -0,0 +1,28 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+class Solution {
+    public boolean isPowerOfThree(double n) {
+        if(n <= 0) {
+            return false;
+        } else if(n == 1) {
+            return true;
+        }
+        
+        return check(n);
+    }
+    
+    public boolean check(double n) {
+        boolean is = false;
+        if(n == 3.0) {
+            is = true;
+        } else if(n < 1) {
+            System.out.println(n);
+            return false;
+        } else {
+            is = isPowerOfThree(n / 3);
+        }
+        return is;
+    }
+}
