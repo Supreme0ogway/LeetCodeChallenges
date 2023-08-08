c8bb808b91883fa47b15591a39411852aa6e228d
William Lattus <wslattus@gmail.com>
2023-08-08 13:42:06 -0700

    notes

--- JavaChallenges/ClimbingStairs.java
+++ JavaChallenges/ClimbingStairs.java
@@ -8,18 +8,21 @@
 
 class Solution {
     public int climbStairs(int n) {
-        if(n <=2) {
+        if(n <=2) {//same pattern to 3
             return n;
         }
         
+        //first 2 but no need for index 0
         int[] s = new int[n + 1];
         s[1] = 1;
         s[2] = 2;
         
+        //f(x) = i-1 + i-2
         for(int i = 3; i <= n; i++) {
             s[i] = s[i-1] + s[i-2];
         }
         
+        //returning the last one (n+1 is size for overflow starting at 1)
         return s[n];
     }
 }
