a392d92e878a593b7b59cf3d73e27c012b1daff2
William Lattus <wslattus@gmail.com>
2024-02-03 10:47:18 -0700

    com

--- PythonChallenges/didItAppearTwice.py
+++
@@ -1,9 +0,0 @@
-************ return true if any element appears twice *******************
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        
-        tempSet = len(set(nums))
-        if(tempSet == len(nums)):
-            return False
-        else:
-            return True
--- PythonChallenges/removeDuplicates.py
+++
@@ -1,9 +0,0 @@
-************ remove duplicates so every element appears once ********
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        index = 1
-        for i in range(1, len(nums)):
-            if(nums[i-1] != nums[i]):
-                nums[index] = nums[i]
-                index = index + 1
-        return index
